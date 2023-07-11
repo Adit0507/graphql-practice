a0d0c883972b0b872af7b5b68d5127ed306456af
aditya-singh-skilzen <aditya.kumar@skilzen.com>
2023-07-12 00:51:48 +0530

    La di da :beers:

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const PORT = 6969
+const graphql = require('graphql')
+const { graphqlHTTP } = require("express-graphql")
+const schema = require('./Schemas/index')
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}))
+
+app.listen(PORT, () => {
+    console.log("Running")
+})
